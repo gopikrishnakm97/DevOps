aca325c3efccbdcbe7a96021b7fb50012c458e11
GopikrishnaKM <141324477+gopikrishnakm97@users.noreply.github.com>
2026-04-25 19:57:07 +0530

    Update main.py

--- llmops-rag-devops-assistant/app/main.py
+++ llmops-rag-devops-assistant/app/main.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from rag_app import ask_question
 
+app = FastAPI()
+
+class QueryRequest(BaseModel):
+    question: str
+
+@app.get("/")
+def root():
+    return {"message": "DevOps AI Assistant is running"}
+
+@app.post("/ask")
+def ask(req: QueryRequest):
+    response = ask_question(req.question)
+    return response
